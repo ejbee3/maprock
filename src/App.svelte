9d69c108374783ccbc62f490b2ea37e3e51b4f33
jo bars <joe.baranski49@gmail.com>
2023-05-31 15:40:22 -0400

    show map

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Map from "./lib/Map.svelte";
+</script>
+
+<main>
+  <h1>this is my MapRock</h1>
+  <section><Map /></section>
+</main>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+  h1 {
+    font-family: "Pacifico", cursive;
+    font-size: 36px;
+  }
+</style>
---
+++ src/lib/Map.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Map from "ol/Map.js";
+  import View from "ol/View.js";
+  import OSM from "ol/source/OSM.js";
+  import TileLayer from "ol/layer/Tile.js";
+  import { onMount } from "svelte";
+  import { Attribution } from "ol/control";
+
+  let mapArea;
+  let osmText;
+
+  onMount(() => {
+    const map = new Map({
+      layers: [new TileLayer({ source: new OSM() })],
+      controls: [],
+      view: new View({
+        center: [0, 0],
+        zoom: 3,
+      }),
+      target: mapArea,
+    });
+    map.addControl(new Attribution({ target: osmText }));
+  });
+</script>
+
+<main>
+  <div bind:this={mapArea} />
+  <p bind:this={osmText} class="osm__text" />
+</main>
+
+<style>
+  div {
+    width: 100%;
+    height: 400px;
+  }
+
+  .osm__text {
+    color: aliceblue;
+    font-size: 18px;
+  }
+</style>
